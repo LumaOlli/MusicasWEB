82505448e8784378b51c9731a0f8a660105cb497
Luma Oliveira <111361940+LumaOlli@users.noreply.github.com>
2024-09-24 17:51:02 -0300

    up

--- musicasweb/src/app/dados/music.ts
+++ musicasweb/src/app/dados/music.ts
@@ -4,6 +4,7 @@ export const musics = [
       id: 1,
       author: "Hungria Hip Hop",
       title: "Amor e Fé",
+      category: "Rap",
       description: "A música 'Amor e Fé' do artista Hungria Hip Hop é uma expressão poética que mistura elementos do cotidiano, sonhos e sentimentos intensos. ",
       image: "/image/amorefe.jpg",
       url: "/audios/amor-e-fe.mp3", // Caminho para o arquivo de áudio
@@ -12,6 +13,7 @@ export const musics = [
       id: 2,
       author: "Caetano Veloso",
       title: "Leaozinho",
+      category: "Pop",
       description: "A canção 'O Leãozinho', composta e interpretada por Caetano Veloso, é uma das mais doces e carinhosas obras do artista, que se destaca por sua poesia e melodia suave.",
       image: "/image/Leaozinho.jpg",
       url: "/audios/leaozinho.mp3", // Caminho para o arquivo de áudio
@@ -20,8 +22,153 @@ export const musics = [
       id: 3,
       author: "Legião Urbana",
       title: "Pais e Filhos",
+      category: "MPB",
       description: "A música 'Pais e Filhos', da banda brasileira Legião Urbana, é uma das mais emblemáticas do rock nacional e aborda com profundidade as relações familiares, a incompreensão entre gerações e a efemeridade da vida.",
       image: "/image/paiefilho.jpg",
       url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
-    }
+    },
+    {
+      id: 4,
+      author: "Alok",
+      title: "Liberdade(Quando Grave Bate Forte)",
+      category: "Eletronica",
+      description: "A música 'Liberdade (Quando Grave Bate Forte)', uma colaboração entre Alok, MC Don Juan e DJ GBR, é uma celebração da liberdade e da alegria que segue o fim de um relacionamento.",
+      image: "/image/Liberdade.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 5,
+      author: "Avicii",
+      title: "Wake Me Up",
+      category: "Eletronica",
+      description: "A música 'Wake Me Up', interpretada por Aloe Blacc e produzida pelo DJ e produtor sueco Avicii, é uma poderosa fusão de elementos eletrônicos com influências do folk e do country.",
+      image: "/image/WakeMeUp.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 6,
+      author: "Queen",
+      title: "We Will Rock You",
+      category: "Rock",
+      description: "A icônica música 'We Will Rock You' do Queen é um hino de estádio conhecido mundialmente, que transcendeu o contexto de sua criação para se tornar um símbolo de desafio e triunfo.",
+      image: "/image/WeWillRockYou.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 7,
+      author: "Bob Dylan",
+      title: "Like a Rolling Stone",
+      category: "Rock",
+      description: "A música 'Like A Rolling Stone', escrita por Bob Dylan e frequentemente confundida com a banda The Rolling Stones devido ao título, é uma das canções mais emblemáticas da década de 60 e um marco na carreira de Dylan.",
+      image: "/image/LikeRollingStone.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 8,
+      author: "Frank Sinatra",
+      title: "Fly me to the moom",
+      category: "Jazz",
+      description: "A canção 'Fly Me To The Moon', imortalizada na voz de Frank Sinatra, é um clássico que transcende gerações com sua melodia suave e letra romântica.",
+      image: "/image/FlyMeToTheMoom.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 9,
+      author: "Louis Armstrong",
+      title: "What a wonderful world",
+      category: "Jazz",
+      description: "A canção 'What a Wonderful World', interpretada pela voz inconfundível de Louis Armstrong, é um hino de otimismo e apreciação pelas maravilhas do cotidiano.",
+      image: "/image/WhatWonderfulWorld.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 10,
+      author: "Charlie Brown Jr.",
+      title: "Só os loucos sabem",
+      category: "Pop",
+      description: "A música 'Só Os Loucos Sabem', da banda Charlie Brown Jr., é uma composição que reflete sobre a vida, as escolhas e a busca por paz interior.",
+      image: "/image/SosLoucosSabem.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 11,
+      author: "Mc Chefin",
+      title: "212",
+      category: "Hip-hop",
+      description: "A música '212' do artista Chefin é uma celebração da ascensão pessoal e profissional, marcada por uma forte expressão de autenticidade e resiliência. ",
+      image: "/image/212.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 12,
+      author: "Gloria Groove",
+      title: "Leilão",
+      category: "Hip-hop",
+      description: "A música 'LEILÃO' de Gloria Groove é uma celebração da autoestima e do poder pessoal, onde a artista se compara a uma obra de arte valiosa, desejada e disputada em um leilão.",
+      image: "/image/Leilao.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 14,
+      author: "Antonio Vivaldi",
+      title: "A primavera",
+      category: "Musica Classica",
+      description: "A 'Primavera', parte da obra 'As Quatro Estações' de Antonio Vivaldi, é uma das peças mais emblemáticas da música clássica.",
+      image: "/image/Aprimavera.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 15,
+      author: "Tchaikovsky",
+      title: "A Quinta Sinfonia de Tchaikovsky",
+      category: "Musica Classica",
+      description: "A 5ª Sinfonia de Ludwig Van Beethoven é uma das obras mais emblemáticas e reconhecidas da música clássica. ",
+      image: "/image/5sinfonia.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 16,
+      author: "Bob Marley",
+      title: "Is This Love?",
+      category: "Raggae",
+      description: "A música 'Is This Love?' de Bob Marley é um clássico do reggae que transcende o tempo com sua mensagem universal sobre o amor e a união.",
+      image: "/image/IsThisLove.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 17,
+      author: "Travie McCoy",
+      title: "Billionaire",
+      category: "Raggae",
+      description: "A música também aborda a ideia de compartilhar a riqueza com aqueles que o artista ama, garantindo que ninguém ao seu redor passe fome.",
+      image: "/image/Billionaire.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 18,
+      author: "Tom Jobim",
+      title: "Águas De Março",
+      category: "MPB",
+      description: "A canção 'Águas de Março', composta por Antônio Carlos Jobim, mais conhecido como Tom Jobim, é uma das obras-primas da música popular brasileira.",
+      image: "/image/ÁguasDeMarço.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 19,
+      author: "Zé neto e Cristiano",
+      title: "Oi balde",
+      category: "Sertanejo",
+      description: "A música 'Oi Balde', interpretada pela dupla sertaneja Zé Neto e Cristiano, aborda um tema bastante comum na música sertaneja: o arrependimento após o término de um relacionamento. ",
+      image: "/image/oiBalde.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
+    {
+      id: 19,
+      author: "Hugo e Guilherme",
+      title: "Mal feito",
+      category: "Sertanejo",
+      description: "A música 'Mal Feito' de Hugo e Guilherme, com participação de Marília Mendonça, é um desabafo emocional que aborda a descoberta de uma traição em um relacionamento.",
+      image: "/image/MalFeito.jpg",  // Caminho para o arquivo de imagem
+      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+    },
 ];  
--- musicasweb/src/app/page.tsx
+++ musicasweb/src/app/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
   const { playing, configPlayPause, playMusic, nextTrack, prevTrack, progress, seek, currentMusic, setVolume } = useContext(HomeContext);
   const [currentMusicData, setCurrentMusicData] = useState<{ title: string; image: string; author: string; description: string } | null>(null);
   const [audioDuration, setAudioDuration] = useState<number | null>(null);
-  const [volume, setLocalVolume] = useState(1); // Controle de volume local
+  const [volume, setLocalVolume] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null); // Categoria selecionada
 
   useEffect(() => {
     if (currentMusic) {
@@ -23,61 +24,76 @@ export default function Home() {
     }
   }, [currentMusic]);
 
-  // Atualiza o volume no contexto e localmente
   const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newVolume = parseFloat(e.target.value);
     setLocalVolume(newVolume);
-    setVolume(newVolume); // Atualiza o volume no contexto de áudio
+    setVolume(newVolume);
   };
 
-  // Escolhe o ícone de volume com base no nível atual
   const renderVolumeIcon = () => {
     if (volume === 0) return <FaVolumeMute className="text-[30px]" />;
     if (volume > 0 && volume <= 0.5) return <FaVolumeDown className="text-[30px]" />;
     return <FaVolumeUp className="text-[30px]" />;
   };
 
+  // Categorias extraídas das músicas
+  const categories = Array.from(new Set(musics.map(music => music.category)));
+
   return (
     <main className="flex min-h-screen flex-row p-24">
-      {/* Menu lateral - músicas no lado esquerdo */}
+      {/* Menu lateral - categorias de músicas */}
       <aside className="w-1/4 p-4 border-r border-gray-200 flex flex-col items-center">
         <hr className="border-gray-300 w-full mb-4" />
-        <h2 className="text-lg font-bold mb-4 text-center">Músicas Disponíveis</h2>
+        <h2 className="text-lg font-bold mb-4 text-center">Categorias</h2>
         <hr className="border-gray-300 w-full mb-4" />
         <ul className="w-full">
-          {musics.map((music) => (
+          {categories.map((category) => (
             <li
-              key={music.id}
-              className="mb-6 border-b border-gray-300 pb-4 cursor-pointer flex flex-col items-center"
-              onClick={() => playMusic(music.url)}
+              key={category}
+              className={`mb-4 pb-2 cursor-pointer text-center ${selectedCategory === category ? 'font-bold text-blue-500' : ''}`}
+              onClick={() => setSelectedCategory(category)}
             >
-              <img src={music.image} alt={music.title} className="w-40 h-40 object-cover mb-2" />
-              <h3 className="text-xl font-semibold mb-1">{music.title}</h3>
-              <p className="text-sm text-gray-500 mb-1">Artist: {music.author}</p>
-              <p className="text-center">{music.description}</p>
+              {category}
             </li>
           ))}
         </ul>
       </aside>
 
-      {/* Conteúdo principal - ao lado direito */}
+      {/* Lista de músicas no lado direito */}
+      <aside className="w-1/4 p-4 border-l border-gray-200 flex flex-col items-center">
+        <hr className="border-gray-300 w-full mb-4" />
+        <h2 className="text-lg font-bold mb-4 text-center">Músicas</h2>
+        <hr className="border-gray-300 w-full mb-4" />
+        <ul className="w-full">
+          {musics
+            .filter(music => selectedCategory === null || music.category === selectedCategory)
+            .map((music) => (
+              <li
+                key={music.id}
+                className="mb-6 border-b border-gray-300 pb-4 cursor-pointer flex flex-col items-center"
+                onClick={() => playMusic(music.url)}
+              >
+                <img src={music.image} alt={music.title} className="w-40 h-40 object-cover mb-2" />
+                <h3 className="text-xl font-semibold mb-1">{music.title}</h3>
+                <p className="text-sm text-gray-500 mb-1">Artist: {music.author}</p>
+                <p className="text-center">{music.description}</p>
+              </li>
+            ))}
+        </ul>
+      </aside>
+
+      {/* Conteúdo principal */}
       <div className="flex-1 flex flex-col items-center justify-center">
         <div className="flex flex-col items-center mb-6">
-          {/* Coluna com imagem e título da música */}
           <div className="flex flex-col items-center">
             <h1 className="text-2xl font-bold mb-2 text-center">
               {currentMusicData ? currentMusicData.title : 'Clique em cima de uma música para que comece a tocar'}
             </h1>
             {currentMusicData && (
-              <img
-                src={currentMusicData.image}
-                alt={currentMusicData.title}
-                className="w-80 h-80 object-cover mb-2"
-              />
+              <img src={currentMusicData.image} alt={currentMusicData.title} className="w-80 h-80 object-cover mb-2" />
             )}
           </div>
 
-          {/* Controle de Volume horizontal */}
           <div className="w-full flex justify-center items-center mb-4">
             {renderVolumeIcon()}
             <input
@@ -87,11 +103,10 @@ export default function Home() {
               step="0.01"
               value={volume}
               onChange={handleVolumeChange}
-              className="volume-slider w-40 h-2 bg-gray-200 rounded-lg cursor-pointer" // Reduzido o tamanho do controle de volume
+              className="volume-slider w-40 h-2 bg-gray-200 rounded-lg cursor-pointer"
             />
           </div>
 
-          {/* Barra de progresso e tempo */}
           {currentMusic && (
             <div className="w-full flex flex-col items-center">
               <input
@@ -101,7 +116,7 @@ export default function Home() {
                 step="0.1"
                 value={progress}
                 onChange={(e) => seek(parseFloat(e.target.value))}
-                className="w-full h-4 bg-gray-200 rounded-lg cursor-pointer" // Aumentado a barra de progresso
+                className="w-full h-4 bg-gray-200 rounded-lg cursor-pointer"
               />
               <div className="flex justify-between w-full mt-2 text-sm text-gray-500">
                 <span>{Math.floor(progress / 60)}:{Math.floor(progress % 60).toString().padStart(2, '0')}</span>
