3b2f0939aea981b07f87385346b07775265c3a0f
Luma Oliveira <111361940+LumaOlli@users.noreply.github.com>
2024-09-25 09:26:29 -0300

    up

--- musicasweb/src/app/dados/music.ts
+++ musicasweb/src/app/dados/music.ts
@@ -34,7 +34,7 @@ export const musics = [
       category: "Eletronica",
       description: "A música 'Liberdade (Quando Grave Bate Forte)', uma colaboração entre Alok, MC Don Juan e DJ GBR, é uma celebração da liberdade e da alegria que segue o fim de um relacionamento.",
       image: "/image/Liberdade.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/Liberdade.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 5,
@@ -43,7 +43,7 @@ export const musics = [
       category: "Eletronica",
       description: "A música 'Wake Me Up', interpretada por Aloe Blacc e produzida pelo DJ e produtor sueco Avicii, é uma poderosa fusão de elementos eletrônicos com influências do folk e do country.",
       image: "/image/WakeMeUp.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/Wake_Me_Up.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 6,
@@ -52,7 +52,7 @@ export const musics = [
       category: "Rock",
       description: "A icônica música 'We Will Rock You' do Queen é um hino de estádio conhecido mundialmente, que transcendeu o contexto de sua criação para se tornar um símbolo de desafio e triunfo.",
       image: "/image/WeWillRockYou.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/WeWillRockYou.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 7,
@@ -61,7 +61,7 @@ export const musics = [
       category: "Rock",
       description: "A música 'Like A Rolling Stone', escrita por Bob Dylan e frequentemente confundida com a banda The Rolling Stones devido ao título, é uma das canções mais emblemáticas da década de 60 e um marco na carreira de Dylan.",
       image: "/image/LikeRollingStone.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/LikeRollingStone.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 8,
@@ -70,7 +70,7 @@ export const musics = [
       category: "Jazz",
       description: "A canção 'Fly Me To The Moon', imortalizada na voz de Frank Sinatra, é um clássico que transcende gerações com sua melodia suave e letra romântica.",
       image: "/image/FlyMeToTheMoom.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/FlyMeToTheMoom.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 9,
@@ -79,7 +79,7 @@ export const musics = [
       category: "Jazz",
       description: "A canção 'What a Wonderful World', interpretada pela voz inconfundível de Louis Armstrong, é um hino de otimismo e apreciação pelas maravilhas do cotidiano.",
       image: "/image/WhatWonderfulWorld.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/WHATWONDERFULWORLD.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 10,
@@ -88,7 +88,7 @@ export const musics = [
       category: "Pop",
       description: "A música 'Só Os Loucos Sabem', da banda Charlie Brown Jr., é uma composição que reflete sobre a vida, as escolhas e a busca por paz interior.",
       image: "/image/SosLoucosSabem.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/SosLoucosSabem.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 11,
@@ -97,7 +97,7 @@ export const musics = [
       category: "Hip-hop",
       description: "A música '212' do artista Chefin é uma celebração da ascensão pessoal e profissional, marcada por uma forte expressão de autenticidade e resiliência. ",
       image: "/image/212.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/212.mp3", // Caminho para o arquivo de áudio(sem musica)
     },
     {
       id: 12,
@@ -106,7 +106,7 @@ export const musics = [
       category: "Hip-hop",
       description: "A música 'LEILÃO' de Gloria Groove é uma celebração da autoestima e do poder pessoal, onde a artista se compara a uma obra de arte valiosa, desejada e disputada em um leilão.",
       image: "/image/Leilao.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/leilao.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 14,
@@ -115,7 +115,7 @@ export const musics = [
       category: "Musica Classica",
       description: "A 'Primavera', parte da obra 'As Quatro Estações' de Antonio Vivaldi, é uma das peças mais emblemáticas da música clássica.",
       image: "/image/Aprimavera.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/APrimavera.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 15,
@@ -124,7 +124,7 @@ export const musics = [
       category: "Musica Classica",
       description: "A 5ª Sinfonia de Ludwig Van Beethoven é uma das obras mais emblemáticas e reconhecidas da música clássica. ",
       image: "/image/5sinfonia.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/5sinfonia.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 16,
@@ -133,7 +133,7 @@ export const musics = [
       category: "Raggae",
       description: "A música 'Is This Love?' de Bob Marley é um clássico do reggae que transcende o tempo com sua mensagem universal sobre o amor e a união.",
       image: "/image/IsThisLove.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/IsThisLove.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 17,
@@ -142,7 +142,7 @@ export const musics = [
       category: "Raggae",
       description: "A música também aborda a ideia de compartilhar a riqueza com aqueles que o artista ama, garantindo que ninguém ao seu redor passe fome.",
       image: "/image/Billionaire.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/Billionaire.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 18,
@@ -151,7 +151,7 @@ export const musics = [
       category: "MPB",
       description: "A canção 'Águas de Março', composta por Antônio Carlos Jobim, mais conhecido como Tom Jobim, é uma das obras-primas da música popular brasileira.",
       image: "/image/ÁguasDeMarço.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/AguasdeMarço.mp3", // Caminho para o arquivo de áudio
     },
     {
       id: 19,
@@ -160,15 +160,15 @@ export const musics = [
       category: "Sertanejo",
       description: "A música 'Oi Balde', interpretada pela dupla sertaneja Zé Neto e Cristiano, aborda um tema bastante comum na música sertaneja: o arrependimento após o término de um relacionamento. ",
       image: "/image/oiBalde.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/oiBalde.mp3", // Caminho para o arquivo de áudio
     },
     {
-      id: 19,
+      id: 20,
       author: "Hugo e Guilherme",
       title: "Mal feito",
       category: "Sertanejo",
       description: "A música 'Mal Feito' de Hugo e Guilherme, com participação de Marília Mendonça, é um desabafo emocional que aborda a descoberta de uma traição em um relacionamento.",
       image: "/image/MalFeito.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
+      url: "/audios/MalFeito.mp3", // Caminho para o arquivo de áudio
     },
 ];  
