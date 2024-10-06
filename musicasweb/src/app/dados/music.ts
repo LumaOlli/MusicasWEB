20fe159fa5aa980406f0d0c695f467deacc6bd3d
Luma Oliveira <111361940+LumaOlli@users.noreply.github.com>
2024-10-06 20:07:11 -0300

    up

--- musicasweb/src/app/dados/music.ts
+++ musicasweb/src/app/dados/music.ts
@@ -1,174 +1,174 @@
 // dados/music.ts
 export const musics = [
-    {
-      id: 1,
-      author: "Hungria Hip Hop",
-      title: "Amor e Fé",
-      category: "Rap",
-      description: "A música 'Amor e Fé' do artista Hungria Hip Hop é uma expressão poética que mistura elementos do cotidiano, sonhos e sentimentos intensos. ",
-      image: "/image/amorefe.jpg",
-      url: "/audios/amor-e-fe.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 2,
-      author: "Caetano Veloso",
-      title: "Leaozinho",
-      category: "Pop",
-      description: "A canção 'O Leãozinho', composta e interpretada por Caetano Veloso, é uma das mais doces e carinhosas obras do artista, que se destaca por sua poesia e melodia suave.",
-      image: "/image/Leaozinho.jpg",
-      url: "/audios/leaozinho.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 3,
-      author: "Legião Urbana",
-      title: "Pais e Filhos",
-      category: "MPB",
-      description: "A música 'Pais e Filhos', da banda brasileira Legião Urbana, é uma das mais emblemáticas do rock nacional e aborda com profundidade as relações familiares, a incompreensão entre gerações e a efemeridade da vida.",
-      image: "/image/paiefilho.jpg",
-      url: "/audios/Pais-e-filhos.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 4,
-      author: "Alok",
-      title: "Liberdade(Quando Grave Bate Forte)",
-      category: "Eletronica",
-      description: "A música 'Liberdade (Quando Grave Bate Forte)', uma colaboração entre Alok, MC Don Juan e DJ GBR, é uma celebração da liberdade e da alegria que segue o fim de um relacionamento.",
-      image: "/image/Liberdade.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Liberdade.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 5,
-      author: "Avicii",
-      title: "Wake Me Up",
-      category: "Eletronica",
-      description: "A música 'Wake Me Up', interpretada por Aloe Blacc e produzida pelo DJ e produtor sueco Avicii, é uma poderosa fusão de elementos eletrônicos com influências do folk e do country.",
-      image: "/image/WakeMeUp.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Wake_Me_Up.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 6,
-      author: "Queen",
-      title: "We Will Rock You",
-      category: "Rock",
-      description: "A icônica música 'We Will Rock You' do Queen é um hino de estádio conhecido mundialmente, que transcendeu o contexto de sua criação para se tornar um símbolo de desafio e triunfo.",
-      image: "/image/WeWillRockYou.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/WeWillRockYou.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 7,
-      author: "Bob Dylan",
-      title: "Like a Rolling Stone",
-      category: "Rock",
-      description: "A música 'Like A Rolling Stone', escrita por Bob Dylan e frequentemente confundida com a banda The Rolling Stones devido ao título, é uma das canções mais emblemáticas da década de 60 e um marco na carreira de Dylan.",
-      image: "/image/LikeRollingStone.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/LikeRollingStone.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 8,
-      author: "Frank Sinatra",
-      title: "Fly me to the moom",
-      category: "Jazz",
-      description: "A canção 'Fly Me To The Moon', imortalizada na voz de Frank Sinatra, é um clássico que transcende gerações com sua melodia suave e letra romântica.",
-      image: "/image/FlyMeToTheMoom.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/FlyMeToTheMoon.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 9,
-      author: "Louis Armstrong",
-      title: "What a wonderful world",
-      category: "Jazz",
-      description: "A canção 'What a Wonderful World', interpretada pela voz inconfundível de Louis Armstrong, é um hino de otimismo e apreciação pelas maravilhas do cotidiano.",
-      image: "/image/WhatWonderfulWorld.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/WHATWONDERFULWORLD.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 10,
-      author: "Charlie Brown Jr.",
-      title: "Só os loucos sabem",
-      category: "Pop",
-      description: "A música 'Só Os Loucos Sabem', da banda Charlie Brown Jr., é uma composição que reflete sobre a vida, as escolhas e a busca por paz interior.",
-      image: "/image/SosLoucosSabem.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/SosLoucosSabem.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 11,
-      author: "Mc Chefin",
-      title: "212",
-      category: "Hip-hop",
-      description: "A música '212' do artista Chefin é uma celebração da ascensão pessoal e profissional, marcada por uma forte expressão de autenticidade e resiliência. ",
-      image: "/image/212.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/212.mp3", // Caminho para o arquivo de áudio(sem musica)
-    },
-    {
-      id: 12,
-      author: "Gloria Groove",
-      title: "Leilão",
-      category: "Hip-hop",
-      description: "A música 'LEILÃO' de Gloria Groove é uma celebração da autoestima e do poder pessoal, onde a artista se compara a uma obra de arte valiosa, desejada e disputada em um leilão.",
-      image: "/image/Leilao.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/leilao.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 14,
-      author: "Antonio Vivaldi",
-      title: "A primavera",
-      category: "Musica Classica",
-      description: "A 'Primavera', parte da obra 'As Quatro Estações' de Antonio Vivaldi, é uma das peças mais emblemáticas da música clássica.",
-      image: "/image/Aprimavera.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/APrimavera.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 15,
-      author: "Tchaikovsky",
-      title: "A Quinta Sinfonia de Tchaikovsky",
-      category: "Musica Classica",
-      description: "A 5ª Sinfonia de Ludwig Van Beethoven é uma das obras mais emblemáticas e reconhecidas da música clássica. ",
-      image: "/image/5sinfonia.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/5sinfonia.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 16,
-      author: "Bob Marley",
-      title: "Is This Love?",
-      category: "Raggae",
-      description: "A música 'Is This Love?' de Bob Marley é um clássico do reggae que transcende o tempo com sua mensagem universal sobre o amor e a união.",
-      image: "/image/IsThisLove.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/IsThisLove.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 17,
-      author: "Travie McCoy",
-      title: "Billionaire",
-      category: "Raggae",
-      description: "A música também aborda a ideia de compartilhar a riqueza com aqueles que o artista ama, garantindo que ninguém ao seu redor passe fome.",
-      image: "/image/Billionaire.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/Billionaire.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 18,
-      author: "Tom Jobim",
-      title: "Águas De Março",
-      category: "MPB",
-      description: "A canção 'Águas de Março', composta por Antônio Carlos Jobim, mais conhecido como Tom Jobim, é uma das obras-primas da música popular brasileira.",
-      image: "/image/ÁguasDeMarço.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/AguasdeMarço.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 19,
-      author: "Zé neto e Cristiano",
-      title: "Oi balde",
-      category: "Sertanejo",
-      description: "A música 'Oi Balde', interpretada pela dupla sertaneja Zé Neto e Cristiano, aborda um tema bastante comum na música sertaneja: o arrependimento após o término de um relacionamento. ",
-      image: "/image/oiBalde.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/oiBalde.mp3", // Caminho para o arquivo de áudio
-    },
-    {
-      id: 20,
-      author: "Hugo e Guilherme",
-      title: "Mal feito",
-      category: "Sertanejo",
-      description: "A música 'Mal Feito' de Hugo e Guilherme, com participação de Marília Mendonça, é um desabafo emocional que aborda a descoberta de uma traição em um relacionamento.",
-      image: "/image/MalFeito.jpg",  // Caminho para o arquivo de imagem
-      url: "/audios/MalFeito.mp3", // Caminho para o arquivo de áudio
-    },
+  {
+    id: 1,
+    author: "Hungria Hip Hop",
+    title: "Amor e Fé",
+    category: "Rap",
+    description: "A música 'Amor e Fé' do artista Hungria Hip Hop é uma expressão poética que mistura elementos do cotidiano, sonhos e sentimentos intensos. ",
+    image: "/image/amorefe.jpg",
+    url: "/audios/amor-e-fe.mp3",
+  },
+  {
+    id: 2,
+    author: "Caetano Veloso",
+    title: "Leaozinho",
+    category: "Pop",
+    description: "A canção 'O Leãozinho', composta e interpretada por Caetano Veloso, é uma das mais doces e carinhosas obras do artista, que se destaca por sua poesia e melodia suave.",
+    image: "/image/Leaozinho.jpg",
+    url: "/audios/leaozinho.mp3",
+  },
+  {
+    id: 3,
+    author: "Legião Urbana",
+    title: "Pais e Filhos",
+    category: "MPB",
+    description: "A música 'Pais e Filhos', da banda brasileira Legião Urbana, é uma das mais emblemáticas do rock nacional e aborda com profundidade as relações familiares, a incompreensão entre gerações e a efemeridade da vida.",
+    image: "/image/paiefilho.jpg",
+    url: "/audios/Pais-e-filhos.mp3",
+  },
+  {
+    id: 4,
+    author: "Alok",
+    title: "Liberdade(Quando Grave Bate Forte)",
+    category: "Eletronica",
+    description: "A música 'Liberdade (Quando Grave Bate Forte)', uma colaboração entre Alok, MC Don Juan e DJ GBR, é uma celebração da liberdade e da alegria que segue o fim de um relacionamento.",
+    image: "/image/Liberdade.jpg",  
+    url: "/audios/Liberdade.mp3", 
+  },
+  {
+    id: 5,
+    author: "Avicii",
+    title: "Wake Me Up",
+    category: "Eletronica",
+    description: "A música 'Wake Me Up', interpretada por Aloe Blacc e produzida pelo DJ e produtor sueco Avicii, é uma poderosa fusão de elementos eletrônicos com influências do folk e do country.",
+    image: "/image/WakeMeUp.jpg",  
+    url: "/audios/Wake_Me_Up.mp3", 
+  },
+  {
+    id: 6,
+    author: "Queen",
+    title: "We Will Rock You",
+    category: "Rock",
+    description: "A icônica música 'We Will Rock You' do Queen é um hino de estádio conhecido mundialmente, que transcendeu o contexto de sua criação para se tornar um símbolo de desafio e triunfo.",
+    image: "/image/WeWillRockYou.jpg",  
+    url: "/audios/WeWillRockYou.mp3",
+  },
+  {
+    id: 7,
+    author: "Bob Dylan",
+    title: "Like a Rolling Stone",
+    category: "Rock",
+    description: "A música 'Like A Rolling Stone', escrita por Bob Dylan e frequentemente confundida com a banda The Rolling Stones devido ao título, é uma das canções mais emblemáticas da década de 60 e um marco na carreira de Dylan.",
+    image: "/image/LikeRollingStone.jpg",  
+    url: "/audios/LikeRollingStone.mp3",
+  },
+  {
+    id: 8,
+    author: "Frank Sinatra",
+    title: "Fly Me To The Moom",
+    category: "Jazz",
+    description: "A canção 'Fly Me To The Moon', imortalizada na voz de Frank Sinatra, é um clássico que transcende gerações com sua melodia suave e letra romântica.",
+    image: "/image/FlyMeToTheMoom.jpg",  
+    url: "/audios/FlyMeToTheMoon.mp3", 
+  },
+  {
+    id: 9,
+    author: "Louis Armstrong",
+    title: "What a Wonderful World",
+    category: "Jazz",
+    description: "A canção 'What a Wonderful World', interpretada pela voz inconfundível de Louis Armstrong, é um hino de otimismo e apreciação pelas maravilhas do cotidiano.",
+    image: "/image/WhatWonderfulWorld.jpg",  
+    url: "/audios/WHATWONDERFULWORLD.mp3", 
+  },
+  {
+    id: 10,
+    author: "Charlie Brown Jr.",
+    title: "Só Os Loucos Sabem",
+    category: "Pop",
+    description: "A música 'Só Os Loucos Sabem', da banda Charlie Brown Jr., é uma composição que reflete sobre a vida, as escolhas e a busca por paz interior.",
+    image: "/image/SosLoucosSabem.jpg",  
+    url: "/audios/SosLoucosSabem.mp3", 
+  },
+  {
+    id: 11,
+    author: "Mc Chefin",
+    title: "212",
+    category: "Hip-hop",
+    description: "A música '212' do artista Chefin é uma celebração da ascensão pessoal e profissional, marcada por uma forte expressão de autenticidade e resiliência. ",
+    image: "/image/212.jpg",  
+    url: "/audios/212.mp3", 
+  },
+  {
+    id: 12,
+    author: "Gloria Groove",
+    title: "Leilão",
+    category: "Hip-hop",
+    description: "A música 'LEILÃO' de Gloria Groove é uma celebração da autoestima e do poder pessoal, onde a artista se compara a uma obra de arte valiosa, desejada e disputada em um leilão.",
+    image: "/image/Leilao.jpg",  
+    url: "/audios/leilao.mp3", 
+  },
+  {
+    id: 13,
+    author: "Antonio Vivaldi",
+    title: "A Primavera",
+    category: "Musica Classica",
+    description: "A 'Primavera', parte da obra 'As Quatro Estações' de Antonio Vivaldi, é uma das peças mais emblemáticas da música clássica.",
+    image: "/image/Aprimavera.jpg",  
+    url: "/audios/APrimavera.mp3", 
+  },
+  {
+    id: 14,
+    author: "Tchaikovsky",
+    title: "A Quinta Sinfonia de Tchaikovsky",
+    category: "Musica Classica",
+    description: "A 5ª Sinfonia de Ludwig Van Beethoven é uma das obras mais emblemáticas e reconhecidas da música clássica. ",
+    image: "/image/5sinfonia.jpg",  
+    url: "/audios/5sinfonia.mp3", 
+  },
+  {
+    id: 15,
+    author: "Bob Marley",
+    title: "Is This Love?",
+    category: "Raggae",
+    description: "A música 'Is This Love?' de Bob Marley é um clássico do reggae que transcende o tempo com sua mensagem universal sobre o amor e a união.",
+    image: "/image/IsThisLove.jpg",  
+    url: "/audios/IsThisLove.mp3", 
+  },
+  {
+    id: 16,
+    author: "Travie McCoy",
+    title: "Billionaire",
+    category: "Raggae",
+    description: "A música também aborda a ideia de compartilhar a riqueza com aqueles que o artista ama, garantindo que ninguém ao seu redor passe fome.",
+    image: "/image/Billionaire.jpg",  
+    url: "/audios/Billionaire.mp3", 
+  },
+  {
+    id: 17,
+    author: "Tom Jobim",
+    title: "Águas De Março",
+    category: "MPB",
+    description: "A canção 'Águas de Março', composta por Antônio Carlos Jobim, mais conhecido como Tom Jobim, é uma das obras-primas da música popular brasileira.",
+    image: "/image/ÁguasDeMarço.jpg",  
+    url: "/audios/AguasdeMarço.mp3", 
+  },
+  {
+    id: 18,
+    author: "Zé neto e Cristiano",
+    title: "Oi Balde",
+    category: "Sertanejo",
+    description: "A música 'Oi Balde', interpretada pela dupla sertaneja Zé Neto e Cristiano, aborda um tema bastante comum na música sertaneja: o arrependimento após o término de um relacionamento. ",
+    image: "/image/oiBalde.jpg",  
+    url: "/audios/oiBalde.mp3", 
+  },
+  {
+    id: 19,
+    author: "Hugo e Guilherme",
+    title: "Mal Feito",
+    category: "Sertanejo",
+    description: "A música 'Mal Feito' de Hugo e Guilherme, com participação de Marília Mendonça, é um desabafo emocional que aborda a descoberta de uma traição em um relacionamento.",
+    image: "/image/MalFeito.jpg",  
+    url: "/audios/MalFeito.mp3", 
+  },
 ];  
